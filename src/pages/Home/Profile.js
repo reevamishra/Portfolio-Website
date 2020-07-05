9f659f4647dde3dcec46c31da5c244c0f076f581
reevamishra <reevamishra208@gmail.com>
2020-07-05 17:17:28 +0530

    Add files via upload

--- src/pages/Home/Profile.js
+++ src/pages/Home/Profile.js
@@ -23,10 +23,10 @@ const ProfileText = ({ status, titleId }) => (
       />
     </ProfileTitle>
     <ProfileDescription status={status}>
-      I’m Cody. Currently, I am based in Austin, working as the lead designer at <Anchor as={Link} to="/projects/dtt">DevTechTools</Anchor>. I am a designer, full-stack developer, and creator of web & mobile solutions with a focus on motion and user experience.
+    Namita Dash & Associates in Bhubaneshwar is one of the leading businesses in the Interior Designers. Also known for Interior Designers, Interior Decorators and much more.
     </ProfileDescription>
     <ProfileDescription status={status}>
-      In my spare time, I like to play guitar and <Anchor href="https://codepen.io/cbenn" target="_blank">experiment with new tech</Anchor>. I’m always interested in new projects, so feel free to drop me a line.
+    Established in the year 2001, Namita Dash & Associates in Chandrasekharpur, Bhubaneshwar is a top player in the category Interior Designers in the Bhubaneshwar. This well-known establishment acts as a one-stop destination servicing customers both local and from other parts of Bhubaneshwar. Over the course of its journey, this business has established a firm foothold in it’s industry. The belief that customer satisfaction is as important as their products and services, have helped this establishment garner a vast base of customers, which continues to grow by the day. This business employs individuals that are dedicated towards their respective roles and put in a lot of effort to achieve the common vision and larger goals of the company. In the near future, this business aims to expand its line of products and services and cater to a larger client base. In Bhubaneshwar, this establishment occupies a prominent location in Chandrasekharpur. It is an effortless task in commuting to this establishment as there are various modes of transport readily available. It is at Kanan Vihar, Near Patia Chowk, which makes it easy for first-time visitors in locating this establishment. It is known to provide top service in the following categories: Interior Designers, Interior Decorators.
     </ProfileDescription>
   </Fragment>
 );
